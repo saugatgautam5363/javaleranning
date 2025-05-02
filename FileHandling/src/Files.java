8ddc12add2ed583f8bad363b673eea73632453f6
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-02 18:38:32 +0545

    file create using file handling

---
+++ FileHandling/src/Files.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Files {
+    public static void main(String[] args) {
+        File file = new File("example.txt");
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created.");
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
