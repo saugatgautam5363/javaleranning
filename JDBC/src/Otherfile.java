3f96b343ab61eb6565f4c6b0180e555fc007b9a3
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-11 22:08:14 +0545

    database connection

---
+++ JDBC/src/Otherfile.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class Otherfile {
+    private static final String url = "jdbc:mysql://localhost:3306/mydb";
+    private static final String userName = "root" ;
+    private static final String password = "admin";
+    public static void main(String[] args) {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            Connection connection = DriverManager.getConnection(url,userName,password);
+            Statement statement = connection.createStatement();
+            String query = "select * from students";
+            ResultSet resultSet = statement.executeQuery(query);
+            while(resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                double marks = resultSet.getDouble("marks");
+                System.out.println("Id: "+id);
+                System.out.println("Name: "+name);
+                System.out.println("age: "+age);
+                System.out.println("Maks: "+marks);
+            }
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
