fe96b79d0d1e0f769ac33a93189ac8993072123c
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-18 09:08:35 +0545

    GUL

---
+++ GLU/src/First.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class First {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Student management System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000,4000);
+
+
+        frame.setVisible(true);
+    }
+}
