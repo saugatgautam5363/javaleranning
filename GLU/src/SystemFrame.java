16daa53135a49e2fd3f309c38892183fc35642c5
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-18 09:08:57 +0545

    GUL

---
+++ GLU/src/SystemFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class SystemFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Student management System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000,4000);
+
+
+        frame.setVisible(true);
+    }
+}
