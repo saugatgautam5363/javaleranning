50d2eee7c2fb9862e4aee9601a8d7c07872dd3a0
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-23 09:45:27 +0545

    GUI

---
+++ GLU/src/BorderLayout.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class BorderLayout {
+    JFrame frame;
+
+    public static void main(String[] args) {
+
+    }
+        public BorderLayout(){
+            frame.setSize(400,200);
+            frame.setVisible(true);
+        }
+
+}
