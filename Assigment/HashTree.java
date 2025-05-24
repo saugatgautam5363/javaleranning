0bf23809ea19c03326b485d76f65bb207d2c7207
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-24 19:35:01 +0545

    hash map

---
+++ Assigment/HashTree.java
@@ -0,0 +1,12 @@
+package Assigment;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class HashTree {
+    public static void main(String[] args) {
+        Map<String,Integer> treemap = new TreeMap<>();
+        treemap.put("R",4);
+        treemap.put("V",5);
+    }
+}
