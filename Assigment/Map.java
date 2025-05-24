83e483fa584d0007463889792680f44fc9041e83
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-24 19:28:12 +0545

    hash map

---
+++ Assigment/Map.java
@@ -0,0 +1,11 @@
+package Assigment;
+
+import java.util.HashMap;
+
+public class Map {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("B",20);
+        hashMap.put("$",4);
+    }
+}
