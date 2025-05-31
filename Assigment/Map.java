29a25ba9e1ec6968ee66f065a049000ade9bdf31
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-31 20:07:04 +0545

    java program

--- Assigment/Map.java
+++ Assigment/Map.java
@@ -6,6 +6,7 @@ public class Map {
     public static void main(String[] args) {
         HashMap<String, Integer> hashMap = new HashMap<>();
         hashMap.put("B",20);
+
         hashMap.put("$",4);
     }
 }
