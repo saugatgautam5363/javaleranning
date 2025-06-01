29a02cccce5b715e158761d4e09dc729916b540f
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-01 17:17:30 +0545

    oop

---
+++ Assigment/Java.java
@@ -0,0 +1,33 @@
+package Assigment;
+
+public class Java {
+    public class Car {
+        int speed;
+        int Brand;
+        int model;
+
+        public int getSpeed() {
+            return speed;
+        }
+
+        public void setSpeed(int speed) {
+            this.speed = speed;
+        }
+
+        public int getBrand() {
+            return Brand;
+        }
+
+        public void setBrand(int brand) {
+            Brand = brand;
+        }
+
+        public int getModel() {
+            return model;
+        }
+
+        public void setModel(int model) {
+            this.model = model;
+        }
+    }
+}
